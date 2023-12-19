b202639c616039ff0ad17b66de17ce26293fc64d
Uirochan <haumuchan@gmail.com>
2023-12-19 23:42:03 +0900

    Cake Script Updated
    stopVFX

--- Assets/Script/CakeScript.cs
+++ Assets/Script/CakeScript.cs
@@ -1,24 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class CakeScript : MonoBehaviour
 {
-    [SerializeField] float xOffset;
-    [SerializeField] float yOffset;
+    private const int maxHealth = 5;
+    private int health;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] VisualEffect[] CandleLights;
+
+
+    private void Start()
     {
-        
+        health = 5;
     }
-
-    // Update is called once per frame
-    void Update()
+    void UnlitCandle()
     {
-        var pos2 = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
-        Vector3 nextPosition = new Vector3(pos2.x + xOffset, pos2.y + yOffset, transform.position.z);
+        int ind = maxHealth - health;
+        if (ind < 0) return;
 
-        transform.position = nextPosition;
+        CandleLights[ind].SendEvent("StopPlay");
     }
 }
