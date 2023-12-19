5277a6c3d9b19716214b5a0d5c9f7868d5c5c28b
Uirochan <haumuchan@gmail.com>
2023-12-19 23:14:58 +0900

    Cake Render

--- Assets/Script/CakeScript.cs
+++ Assets/Script/CakeScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CakeScript : MonoBehaviour
 {
+    [SerializeField] float xOffset;
+    [SerializeField] float yOffset;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class CakeScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var pos2 = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        Vector3 nextPosition = new Vector3(pos2.x + xOffset, pos2.y + yOffset, transform.position.z);
+
+        transform.position = nextPosition;
     }
 }
