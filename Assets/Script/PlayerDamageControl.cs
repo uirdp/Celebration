90b17136e3558447b19b74fb419399db3cdb3157
Uirochan <haumuchan@gmail.com>
2023-12-22 00:27:05 +0900

    Firework event play

--- Assets/Script/PlayerDamageControl.cs
+++ Assets/Script/PlayerDamageControl.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MoreMountains.Feedbacks;
+using UnityEngine.VFX;
 
+//should be renamed to status control
 public class PlayerDamageControl : MonoBehaviour
 {
     public CakeScript cakeScript;
+    public VisualEffect firework;
     public MMF_Player damageFeedback;
 
     private void Awake()
     {
         damageFeedback.Initialization();
+        firework.SendEvent("Stop");
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -18,6 +22,15 @@ public class PlayerDamageControl : MonoBehaviour
         {
             damageFeedback?.PlayFeedbacks();
             cakeScript.UnlitCandle();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Goal")
+        {
+            Debug.Log("fire");
+            firework.SendEvent("Play");
         }
     }
 }
