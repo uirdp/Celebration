542eaecea180d83b6d5a5dba5f4b3f56f890e9c3
Uirochan <haumuchan@gmail.com>
2024-01-01 18:31:55 +0900

    scriptable object attempt

---
+++ Assets/Script/EnemyScript/EnemyData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "EnemyData")]
+public class EnemyData : ScriptableObject
+{
+    public float speed;
+    public float acc;
+
+    public float verticalSpeed;
+    public int dir;
+}
