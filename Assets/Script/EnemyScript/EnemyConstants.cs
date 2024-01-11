e95ef5e7a6ca1d57d123ab9c094ffb83aec02ee4
Uirochan <haumuchan@gmail.com>
2024-01-11 22:24:45 +0900

    Added VFX manager

--- Assets/Script/EnemyScript/EnemyConstants.cs
+++ Assets/Script/EnemyScript/EnemyConstants.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class EnemyConstants : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static readonly float[] spawnPos = { 0.1f, 0.2f };
 }
