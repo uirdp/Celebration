c9f87317cdb3d6e2b464d23dbe306a3421f4384f
Uirochan <haumuchan@gmail.com>
2024-01-12 21:06:57 +0900

    listners

--- Assets/Script/Managers/GameEventListener.cs
+++ Assets/Script/Managers/GameEventListener.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
-    using UnityEngine;
+using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 public class GameEventListener : MonoBehaviour
 {
+    public GameEvent Event;
+    public UnityEvent Response;
+
     private void OnEnable()
     {
-        
+        Event.RegisterListner(this);
     }
 
     private void OnDisable()
     {
-        
+        Event.UnregisterListner(this);
     }
 
     public void OnEventRaised()
     {
-
+        Response.Invoke();
     }
 }
