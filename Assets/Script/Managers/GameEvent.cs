c612ca0d5d5097831ece7e378ce388eb34001d5c
Uirochan <haumuchan@gmail.com>
2024-01-12 21:23:16 +0900

    n

--- Assets/Script/Managers/GameEvent.cs
+++ Assets/Script/Managers/GameEvent.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu]
 public class GameEvent : ScriptableObject
 {
     private List<GameEventListener> listeners =
