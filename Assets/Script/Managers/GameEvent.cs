1f64e57785ad30540ba7fb6f11fb7fad3acc3c37
Uirochan <haumuchan@gmail.com>
2024-01-13 21:55:58 +0900

    adding comments

--- Assets/Script/Managers/GameEvent.cs
+++ Assets/Script/Managers/GameEvent.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//GameEvent has listners, 
 [CreateAssetMenu]
 public class GameEvent : ScriptableObject
 {
     private List<GameEventListener> listeners =
         new List<GameEventListener>();
 
+
+    //when event occured, invokes all listeners
     public void Raise()
     {
         for(int i = listeners.Count - 1; i >= 0; i--)
