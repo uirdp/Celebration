d7862592d5bb43cd288f67c07330a05bca7f99fd
Uirochan <haumuchan@gmail.com>
2024-01-15 20:46:48 +0900

    Spawn data #2

--- Assets/Script/EnemyScript/Enemy Data/EnemySpawnData.cs
+++ Assets/Script/EnemyScript/Enemy Data/EnemySpawnData.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class EnemySpawnData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float[] spawnPointsX;
+    public float[] spawnPointsY;
 }
